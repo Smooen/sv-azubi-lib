563d0936514f632737d146389ffb4eadf8d008b8
Smooen <smooenus@gmail.com>
2024-12-31 09:24:55 +0100

    reworked book listitem a bit

--- src/routes/books/BookListItem.svelte
+++ src/routes/books/BookListItem.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	let { book } = $props();
+	import cover_placeholder from '$lib/assets/cover_placeholder.png';
 
-	$inspect(book);
+	let { book } = $props();
 </script>
 
 <div>
-	<p>this is a book list item {book.title}</p>
-	<a href="/books/{book.isbn}">Book Details</a>
+	<a href="/books/{book.isbn}">
+		<img src="{ book.imageLink ? book.imageLink : cover_placeholder }" alt="Cover image of the book {book.title}" style="width: 18%">
+	</a>
+	<p>Title: {book.title}</p>
+	<p>Author: {book.author}</p>
+	<p>Release Date: {book.releaseDate}</p>
 </div>
